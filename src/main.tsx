286670cae1ed704934ca49c751983fa5bb5092e7
oliveiramiro2 <ramiromares95@gmail.com>
2023-01-24 08:01:53 -0300

    added eslint for patern of code and tailwind to style

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import HomeParallax from "./pages";
+import "./style/global.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <HomeParallax />
+    </React.StrictMode>
+);
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomeParallax: React.FC = () => {
+    return (
+        <div className="w-screen h-screen bg-slate-300 flex justify-center items-center">
+            <span>index</span>
+        </div>
+    );
+};
+
+export default HomeParallax;
